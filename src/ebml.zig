2ad9ef920112686f21b5fa2d8027a961fbefcca3
mschae23 <46165762+mschae23@users.noreply.github.com>
2023-12-12 18:42:47 +0100

    Add readBinary* functions and documentation - TODO: add tests for readBinary* and readDate

--- src/ebml.zig
+++ src/ebml.zig
@@ -2,17 +2,22 @@ const std = @import("std");
 
 const log = std.log.scoped(.ebml);
 
-const io = @import("./io.zig");
-
 pub const EbmlError = error {
     VintTooLarge,
 };
 
+/// The version of EBML supported by this reader.
+///
+/// At the time of writing, only EBML version 1 exists.
 pub const EBML_VERSION: u8 = 1;
+/// The maximum value that can be represented as an EBML variable-sized integer.
+/// As element data sizes are encoded using them, this is also the maximum size a single EBML element can have.
 pub const VINTMAX: u64 = 72_057_594_037_927_934; // 2^56 - 2
 
+/// The maximum depth of nested master elements supported by this EBML reader.
 pub const MAX_NESTING_DEPTH: u8 = 16;
 
+/// Represents an element ID.
 pub const ElementId = struct {
     id: u64,
 };
@@ -22,15 +27,23 @@ pub const MasterElementNestData = struct {
     end_pos: usize,
 };
 
+/// Represents a version of a DocType extension.
 pub const DoctypeExtension = struct {
     name: []const u8, // Owned by EbmlDocument
     version: u32,
 };
 
+/// Represents a single EBML document, and stores information found in the EBML header.
+///
+/// It also holds the [`Reader`] and an optional [`SeekableStream`] of the input byte stream, as well as the current path of nested
+/// EBML master elements.
+///
+/// [`Reader`]: std.io.AnyReader
+/// [`SeekableStream`]: std.io.SeekableStream
 pub const EbmlDocument = struct {
     allocator: std.mem.Allocator,
     reader: std.io.AnyReader,
-    seeker: std.fs.File.SeekableStream,
+    seeker: ?std.fs.File.SeekableStream,
 
     ebml_version: u8, ebml_read_version: u8, // There only exists EBML version 1 at the time of writing
     ebml_max_id_length: u8, // 4 by default, this reader only supports length <=8
@@ -43,12 +56,12 @@ pub const EbmlDocument = struct {
     path_len: u8,
 
     /// Initializes a new EbmlDocument.
-    /// 
+    ///
     /// `seeker` is required to be the `SeekableStream` for `reader`.
-    /// 
+    ///
     /// The provided allocator is only used for runtime-sized data in the EBML header,
     /// not for anything in the EBML body.
-    pub fn init(allocator: std.mem.Allocator, reader: std.io.AnyReader, seeker: std.fs.File.SeekableStream) std.mem.Allocator.Error!EbmlDocument {
+    pub fn init(allocator: std.mem.Allocator, reader: std.io.AnyReader, seeker: ?std.fs.File.SeekableStream) std.mem.Allocator.Error!EbmlDocument {
         return EbmlDocument {
             .allocator = allocator,
             .reader = reader, .seeker = seeker,
@@ -78,11 +91,23 @@ pub const EbmlDocument = struct {
     }
 };
 
+/// Stores the raw, encoded form of a variable-sized integer as a `u64`, i. e. it includes the `VINT_WIDTH` and `VINT_MARKER` BITS,
+/// as well as the number of octets it uses.
+///
+/// [`getVintValue`] can be used to get its integer value.
+///
+/// [`getVintValue`]: getVintValue
 pub const RawVint = struct {
     octets: u8,
     raw: u64,
 };
 
+/// Reads a variable-sized integer, and returns a struct containing the number of octets it uses and the raw value.
+/// [`getVintValue`] can be used to get its integer value.
+///
+/// This EBML reader only supports variable-sized integers of up to 8 octets in length.
+///
+/// [`getVintValue`]: getVintValue
 pub fn readVintRaw(reader: std.io.AnyReader) anyerror!RawVint {
     const byte = try reader.readByte();
     // std.debug.print("Byte: {b}\n", .{byte});
@@ -113,6 +138,15 @@ pub fn readVintRaw(reader: std.io.AnyReader) anyerror!RawVint {
     return RawVint { .octets = octets, .raw = value, };
 }
 
+/// Calculates the actual integer value from a [`RawVint`].
+///
+/// # See also
+/// - [`readVintRaw`]
+/// - [`readVint`]
+///
+/// [`RawVint`]: RawVint
+/// [`readVintRaw`]: readVintRaw
+/// [`readVint`]: readVint
 pub inline fn getVintValue(vint: RawVint) u64 {
     var value = vint.raw;
     // std.debug.print("Value pre-and: {d} ({b})\n", .{value, value});
@@ -122,23 +156,30 @@ pub inline fn getVintValue(vint: RawVint) u64 {
     return value;
 }
 
+// Reads a variable-sized integer.
 pub inline fn readVint(reader: std.io.AnyReader) anyerror!u64 {
     return getVintValue(try readVintRaw(reader));
 }
 
-pub inline fn readElementId(reader: std.io.AnyReader) anyerror!u32 {
+/// Reads an element ID.
+///
+/// **Note**: This function does not check whether the ID is encoded in a valid way, or if an element with that ID even exists.
+pub inline fn readElementId(reader: std.io.AnyReader) anyerror!ElementId {
     // Don't check whether the ID is encoded in an invalid way
-    return try readVint(reader);
+    return ElementId { .id = try readVint(reader) };
 }
 
 pub const UNKNOWN_DATA_SIZE: u64 = std.math.maxInt(u64);
 
+/// Read an element data size, which is encoded as a variable-sized integer in EBML.
+///
+/// If the value specifies an unknown length, this function returns `UNKNOWN_DATA_SIZE` (which currently equals the maximum value of `u64`).
 pub inline fn readElementDataSize(reader: std.io.AnyReader) anyerror!u64 {
     const vint = try readVintRaw(reader);
     const value = getVintValue(vint);
 
     // std.debug.print("Reading element ID (octets: {d}, value: {d}, 0b{b}, 0x{X})\n", .{vint.octets, value, value, value});
-    
+
     if (value == (@as(u64, 1) << @as(u6, @intCast(7 * vint.octets))) - 1) {
         // Element has an unknown data size. It is fine to use 2^64 - 1 (all u64 bits set to 1) as
         // a special indicator for that here, as that is out of bounds of the element data size value.
@@ -148,12 +189,13 @@ pub inline fn readElementDataSize(reader: std.io.AnyReader) anyerror!u64 {
     }
 }
 
-// The following typed read functions assume element ID was already read in the buffer, meaning
-// the buffer position is on the first byte of the element data size
-
+/// Read a signed integer element.
+///
+/// This function requires the element ID to have been read already, i. e. the input stream position
+/// must be on the element size value of the element.
 pub fn readSignedInteger(reader: std.io.AnyReader, default: ?i64) anyerror!i64 {
     const size = try readElementDataSize(reader);
-    
+
     if (size == 0) {
         return default orelse 0;
     } else if (size > 8) {
@@ -168,9 +210,13 @@ pub fn readSignedInteger(reader: std.io.AnyReader, default: ?i64) anyerror!i64 {
     }
 }
 
+/// Read an unsigned integer element.
+///
+/// This function requires the element ID to have been read already, i. e. the input stream position
+/// must be on the element size value of the element.
 pub fn readUnsignedInteger(reader: std.io.AnyReader, default: ?u64) anyerror!u64 {
     const size = try readElementDataSize(reader);
-    
+
     if (size == 0) {
         return default orelse 0;
     } else if (size > 8) {
@@ -185,9 +231,13 @@ pub fn readUnsignedInteger(reader: std.io.AnyReader, default: ?u64) anyerror!u64
     }
 }
 
+/// Read a floating-point integer element.
+///
+/// This function requires the element ID to have been read already, i. e. the input stream position
+/// must be on the element size value of the element.
 pub fn readFloat(reader: std.io.AnyReader, default: ?f64) anyerror!f64 {
     const size = try readElementDataSize(reader);
-    
+
     if (size == 0) {
         return default orelse 0;
     } else if (size == 4) {
@@ -204,6 +254,111 @@ pub fn readFloat(reader: std.io.AnyReader, default: ?f64) anyerror!f64 {
         return error.InvalidElementSize;
     }
 }
+
+/// Reads the entire contents of a binary element into the specified buffer.
+///
+/// If the buffer is not big enough to hold all of the element's data, `error.BufTooSmall` will be returned. In this case, the provided
+/// buffer as well as the current position in the stream will not be modified.
+///
+/// If the element size is zero, the function will return without error and without modifying the provided buffer.
+/// This means that the buffer needs to be filled with the element's default value (usually all zeroes) before calling this.
+///
+/// If the input stream unexpectedly ends before all bytes of the element have been read, `error.EndOfStream` will be returned.
+/// The provided buffer may be partially overwritten with the read data.
+///
+/// Caller owns buffer memory.
+///
+/// This function requires the element ID to have been read already, i. e. the input stream position must be
+/// on the element size value of the element.
+pub fn readBinaryAllBuf(reader: std.io.AnyReader, buffer: []u8) anyerror!void {
+    const size = try readElementDataSize(reader);
+
+    if (size == 0) {
+        return;
+    } else if (size < buffer.len) {
+        return error.BufTooSmall;
+    }
+
+    return reader.readNoEof(buffer);
+}
+
+/// Tries to read the entire contents of a binary element by allocating a buffer of the right size using the provided allocator.
+///
+/// A maximum size has to be provided, as EBML allows binary elements to be practically unbounded in size. If the actual size of the
+/// element turns out to be greater than `max_size`, `error.BufTooSmall` will be returned.
+///
+/// If the input stream unexpectedly ends before all bytes of the element have been read, `error.EndOfStream` will be returned.
+/// All data read as part of the binary element will be lost.
+///
+/// If any error occurs during reading, such as an allocation failure or an IO error, all data read so far as part of the binary element
+/// will be lost. As the reader will now be positioned somewhere within the element with no indication of the number of bytes read,
+/// this state is not easily recoverable.
+///
+/// Caller owns returned memory.
+///
+/// This function requires the element ID to have been read already, i. e. the input stream position must be
+/// on the element size value of the element.
+pub fn readBinaryAllAlloc(reader: std.io.AnyReader, allocator: std.mem.Allocator, max_size: usize) anyerror![]u8 {
+    const size = try readElementDataSize(reader);
+
+    if (size > max_size) {
+        return error.BufTooSmall;
+    }
+
+    const buf = try reader.readAllAlloc(allocator, @min(size, max_size));
+
+    if (buf.len != @min(size, max_size)) { // < @min(size, max_size)
+        return error.EndOfStream;
+    }
+
+    return buf;
+}
+
+/// Read a date element.
+///
+/// Date elements are defined in the EBML specification as follows:
+///
+/// > The date element stores an integer in the same format as the signed
+/// > integer element that expresses a point in time referenced in
+/// > nanoseconds from the precise beginning of the third millennium of the
+/// > Gregorian Calendar in Coordinated Universal Time (also known as
+/// > 2001-01-01T00:00:00.000000000 UTC). This provides a possible
+/// > expression of time from September 1708 to April 2293.
+/// >
+/// > The integer stored represents the number of nanoseconds between the
+/// > date to express and 2001-01-01T00:00:00.000000000 UTC, not counting
+/// > leap seconds. That is 86,400,000,000,000 nanoseconds for each day.
+/// > Conversions from other date systems should ensure leap seconds are
+/// > not counted in EBML values.
+/// >
+/// > The 2001-01-01T00:00:00.000000000 UTC date also corresponds to
+/// > 978307200 seconds in Unix time.
+///
+/// This function is exactly equivalent to [`readSignedInteger`], except that it only allows element sizes of `0` and `8`.
+/// You can use `readSignedInteger` instead, which allows any element size between 0 and 8 (inclusive), if you want to be more
+/// lenient in parsing dates.
+///
+/// This function requires the element ID to have been read already, i. e. the input stream position
+/// must be on the element size value of the element.
+pub fn readDate(reader: std.io.AnyReader, default: ?i64) anyerror!i64 {
+    const size = try readElementDataSize(reader);
+
+    if (size == 0) {
+        return default orelse 0;
+    } else if (size != 8) {
+        return error.InvalidElementSize;
+    } else {
+        var bytes: [8]u8 = .{0} ** 8;
+        try reader.readNoEof(bytes[0..8]);
+        const value = std.mem.bigToNative(i64, std.mem.bytesToValue(i64, &bytes));
+
+        // std.debug.print("0b{b:0>64}, {d}\n", .{value, value});
+        return value;
+    }
+}
+
+// ================
+// Tests
 
 test "readVint (2 in different sizes)" {
     const a: u32 = 0b1000_0010_0000_0000_0000_0000_0000_0000;
